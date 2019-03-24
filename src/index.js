1744af9ead13ef490d895ab3caa2385eedc3725d
Evgeniy Yakichuk <yak.gjen@gmail.com>
2019-03-24 22:51:47 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  const divis = getDivisors(base);
+    let totalImpact = [];
+    let result = [];
+    
+    let i = 0;
+    for (key in divis) {
+      result.push(0);
+      for (let power = 1; number / Math.pow(key, power) > 1; power++) {
+        result[i] += Math.floor(number / Math.pow(key, power));
+      }
+      result[i] = Math.floor(result[i] / divis[key]);
+      i++;
+    }
+    result = Math.min(...result);
+		
+    return result;
+		
+	function getDivisors(base) {
+		let divis = {};
+		for (let startVal = 2; startVal <= base; startVal++) {
+			while (base % startVal === 0) {
+				divis[startVal] ? divis[startVal]++ : divis[startVal] = 1;
+				base /= startVal;
+			}
+		}
+		return divis;
+	}
 }
